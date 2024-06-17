b8d9d00da43e3a0b04a901892e79ce316d4d091c
sandervoorn <sander.voorn@gmail.com>
2024-06-17 17:25:14 +0200

    certificates devices

--- public_html/modules/devices/admin/snippets/pdfBody.inc.php
+++ public_html/modules/devices/admin/snippets/pdfBody.inc.php
@@ -32,30 +32,52 @@
     </tr>
     <tr>
         <td style="width:25%;">Merk:</td>  
-        <td style="width:25%;"><?= _e($oDevice->name) ?></td>
+        <td style="width:25%;"><?= _e($oDevice->brand) ?></td>
         <td style="width:25%;">VBB nummer:</td>  
-        <td style="width:25%;"><?= _e($oUser->name) ?></td> 
+        <td style="width:25%;"><?= _e($oCertificate->vbbNr) ?></td> 
     </tr>
     <tr>
         <td style="width:25%;">Type:</td>  
-        <td style="width:25%;"><?= _e($oDevice->name) ?></td>
+        <td style="width:25%;"><?= _e($oDevice->type) ?></td>
         <td style="width:25%;">Test instrument:</td>  
-        <td style="width:25%;"><?= _e($oUser->name) ?></td> 
+        <td style="width:25%;"><?= _e($oCertificate->testInstrument) ?></td> 
     </tr>
     <tr>
         <td style="width:25%;">Serienummer:</td>  
-        <td style="width:25%;"><?= _e($oDevice->name) ?></td>
+        <td style="width:25%;"><?= _e($oDevice->serial) ?></td>
         <td style="width:25%;">Serienummer:</td>  
-        <td style="width:25%;"><?= _e($oUser->name) ?></td> 
+        <td style="width:25%;"><?= _e($oCertificate->testSerialNr) ?></td> 
     </tr>
     <tr>
         <td style="width:25%;">Test datum:</td>  
         <td style="width:25%;"><?= _e($oDevice->name) ?></td>
         <td style="width:25%;">Volgende keuring:</td>  
-        <td style="width:25%;"><?= _e($oUser->name) ?></td> 
+        <td style="width:25%;"><?= !empty($oCertificate->nextcheck) ? date('d-m-Y', strtotime($oCertificate->nextcheck)) : '' ?></td> 
     </tr>
 </table>
 
-<?php
+<br />
 
-?>
+<table style="width:100%;">
+  <tr>
+    <td style="width:50%;">1. Visuele controle:</td>  
+    <td><?= _e($oCertificate->visualCheck) ?></td>
+  </tr>
+  <tr>
+    <td>2. Weerstand bescherming leiding RPE:</td>
+    <td><?= _e($oCertificate->weerstandBeLeRPE) ?></td>
+  </tr>
+  <tr>
+    <td>3. Isolatie Weerstand RISO:</td>
+    <td><?= _e($oCertificate->isolatieWeRISO) ?></td>
+  </tr>
+  <tr>
+    <td>4. Lekstroom via bescherming leiding IEA:</td>
+    <td><?= _e($oCertificate->lekstroomIEA) ?></td>
+  </tr>
+  <tr>
+    <td>5. Lekstroom door aanraking:</td>
+    <td><?= _e($oCertificate->lekstroomTouch) ?></td>
+  </tr>
+</table>
+
